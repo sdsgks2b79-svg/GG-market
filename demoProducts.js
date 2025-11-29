434e49d0f9288f7fa414cc3391d67bcbee8bc9f1
sdsgks2b79-svg <sdsgks2b79@privaterelay.appleid.com>
2025-11-30 00:32:34 +0500

    Add demoProducts.js with sample product data

---
+++ demoProducts.js
@@ -0,0 +1,8 @@
+const products = [
+    { name: 'Olma', price: 1.5, quality: 'Yaxshi', image: 'https://via.placeholder.com/150' },
+    { name: 'Banan', price: 2.0, quality: 'Yaxshi', image: 'https://via.placeholder.com/150' },
+    { name: 'Sut', price: 1.2, quality: 'Yaxshi', image: 'https://via.placeholder.com/150' },
+    { name: 'Non', price: 0.5, quality: 'Yaxshi', image: 'https://via.placeholder.com/150' }
+];
+
+module.exports = products;
