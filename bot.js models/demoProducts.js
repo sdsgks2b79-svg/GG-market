57afb271ac1f2e352e24b49223dc9f7d5eeb2922
sdsgks2b79-svg <sdsgks2b79@privaterelay.appleid.com>
2025-11-29 21:53:12 +0500

    Add demoProducts data for product listings
    Add demoProducts.js

---
+++ bot.js models/demoProducts.js
@@ -0,0 +1,7 @@
+export const demoProducts = [
+  { name: "Sut 1L", price: 13000, quality: "100% tabiiy", category: "milk", imageUrl: "https://pixabay.com/get/g676c2cb1692b786cd0427eedc2b7a1a64c59160e66c6aef69fe5a7809864780b_1920.jpg" },
+  { name: "Oddiy non", price: 4000, quality: "Yangi pishirilgan", category: "bakery", imageUrl: "https://images.pexels.com/photos/70497/bread-food-baking-70497.jpeg" },
+  { name: "Tovuq file", price: 48000, quality: "Sof, yangi kesilgan", category: "meat", imageUrl: "https://images.pexels.com/photos/842430/pexels-photo-842430.jpeg" },
+  { name: "Olma", price: 15000, quality: "Yangi va shirin meva", category: "fruit", imageUrl: "https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg" },
+  { name: "Cola 1.5L", price: 12000, quality: "Sovuq, paketlangan", category: "drinks", imageUrl: "https://images.pexels.com/photos/109720/pexels-photo-109720.jpeg" }
+];
